cfb81f56a8304d11ce8335442184201acb128465
ahocking <ahocking@pivotal.io>
2018-04-10 12:42:39 -0600

    role_definition_id is required.

--- install-pcf/azure/terraform/c0-azure-multi-res-group/role_definitions.tf
+++ install-pcf/azure/terraform/c0-azure-multi-res-group/role_definitions.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_role_definition" "network_readonly" {
-//  role_definition_id = "00000000-0000-0000-0000-000000000000"
+  role_definition_id = "00000000-0000-0000-0000-000000000000"
   name               = "PCF Read Network Resource Group (custom) ${var.env_name}"
   scope              = "/subscriptions/${var.subscription_id}"
 
@@ -24,7 +24,7 @@ resource "azurerm_role_definition" "network_readonly" {
 }
 
 resource "azurerm_role_definition" "network_deploy" {
-  //  role_definition_id = "00000000-0000-0000-0000-000000000000"
+  role_definition_id = "00000000-0000-0000-0000-000000000000"
   name               = "PCF Deploy Min Perms (custom) ${var.env_name}"
   scope              = "/subscriptions/${var.subscription_id}"
 
@@ -48,7 +48,7 @@ resource "azurerm_role_assignment" "network_readonly_to_pcf_principal" {
 }
 
 resource "azurerm_role_assignment" "network_deploy_to_pcf_principal" {
-  //  name               = "00000000-0000-0000-0000-000000000000"
+//    name               = "00000000-0000-0000-0000-000000000000"
   scope              = "/subscriptions/${var.subscription_id}"
   role_definition_id = "${azurerm_role_definition.network_deploy.id}"
   principal_id       = "${var.pcf_service_principal_client_id}"
